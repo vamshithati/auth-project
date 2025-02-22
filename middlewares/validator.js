09f5d72b2e4b5efabda52fe5bcbb79cfc5b7cfee
vamshithati <vthati@hawk.iit.edu>
2025-02-22 00:15:59 -0600

    validator

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -24,3 +24,43 @@ exports.signinSchema = Joi.object({
 		.required()
 		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
 });
+
+exports.acceptCodeSchema = Joi.object({
+	email: Joi.string()
+		.min(6)
+		.max(60)
+		.required()
+		.email({
+			tlds: { allow: ['com', 'net'] },
+		}),
+	providedCode: Joi.number().required(),
+});
+
+exports.changePasswordSchema = Joi.object({
+	newPassword: Joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+	oldPassword: Joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
+
+exports.acceptFPCodeSchema = Joi.object({
+	email: Joi.string()
+		.min(6)
+		.max(60)
+		.required()
+		.email({
+			tlds: { allow: ['com', 'net'] },
+		}),
+	providedCode: Joi.number().required(),
+	newPassword: Joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
+
+exports.createPostSchema = Joi.object({
+	title: Joi.string().min(3).max(60).required(),
+	description: Joi.string().min(3).max(600).required(),
+	userId: Joi.string().required(),
+});
